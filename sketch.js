d99f1e3edbb50b8dc12e70011bdc19923a17fa57
ElectroFast5 <96546144+ElectroFast5@users.noreply.github.com>
2022-05-17 18:56:26 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var Electrofast, ElectrofastImg
+var level
+
+function preload() {
+  ElectrofastImg = loadAnimation("ElectroFast1_jump.svg", "ElectroFast2.svg", "ElectroFast3.svg", "ElectroFast4.svg", "ElectroFast5_stand.svg");
+}
+
+function setup() {
+  createCanvas(1400,700);
+  //createSprite(200, 200, 50, 50);
+  Electrofast = createSprite(400,400,20,20);
+  Electrofast.addAnimation("Electrofast_Running",ElectrofastImg);
+  Electrofast.scale = 0.5;
+}
+
+function draw() {
+  //background(255,255,255);  
+  background("cyan")
+  drawSprites();
+
+  if(keyDown("RIGHT_ARROW")) {
+    Electrofast.x+=10;
+  }
+
+  if(keyDown("LEFT_ARROW")) {
+    Electrofast.x-=10;
+  }
+
+  if(keyDown("UP_ARROW")) {
+    Electrofast.y-=10
+  }
+
+  if(keyDown("DOWN_ARROW")) {
+    Electrofast.y+=10
+  }
+}
